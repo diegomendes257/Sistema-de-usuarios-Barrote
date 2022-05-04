dd9ce82b125420357d0cefb54bed266c206c54b1
diegomendes257 <printrecife@gmail.com>
2022-05-03 23:30:21 -0300

    insersao pagina comercio

--- comercio.php
+++ comercio.php
@@ -15,10 +15,11 @@
 		$consultaTudo->execute();
 
 		//-------------- EXIBE FOTO -------------
+        $fotoId = 6;
 		$id = $_SESSION['id_usuario'];
-		$consultaFoto = 'SELECT path_perfil from fotosperfil WHERE id_usuario = :id';
+		$consultaFoto = 'SELECT path_perfil from fotosperfil WHERE id_perfil = :id';
 		$consultaFoto = $conexao->prepare($consultaFoto);
-		$consultaFoto->bindValue(':id', $id);
+		$consultaFoto->bindValue(':id', $fotoId);
 		$consultaFoto->execute();
 		$exibeFoto = $consultaFoto->fetch();
 ?>
@@ -56,64 +57,41 @@
 							header('Location: principal.php');
 						}
 					?>
-					
 					<a style="color:yellow" href="inicio.php?sair=true">SAIR</a>
 				</div>
 			</header>
-			<nav style='text-align:center'>
-						<a href="./adm/consulta.php">Clique para consultar!</a> - /
-						<a href="./adm/adicionaValor.php">Clique para creditar dinheiro para alguém!</a> - /
-						<a href="./adm/adicionaValorDado.php">Clique para creditar e ser debitado!</a> - /
-						<a href="formImagem.php">Carregar imagem!</a>
-			</nav>
-			<div class="meio">
-				<section class="container-fluid bg-white banner">
-					<h2>
-						<b>Aqui é o início de tudo!!!</b>
-					</h2>
-					<p>Vá para a aba de salão para se divertir! Ganhe moedas fazendo as tarefas!</p>
-				</section>
-				<section id="dados" class="container-fluid bg-white">
-
-					<style>
-            			.fotoPerfil{
-                				width: 150px;
-								border-radius: 100%;
-           			 		}
-						.textoPerfil{
-								color: white;
-							}
-        			</style>
-
-					<div class="container bg-dark">
-						<p class="textoPerfil">SUA FOTO DE PERFIL</p>
-						<?php
-							echo "<img class='fotoPerfil' src='./".$exibeFoto['path_perfil']."'>";
-						?>
-						<p class="textoPerfil"><a href="trocaFotoPerfil.php">TROCAR FOTO DE PERFIL</a></p>
-					</div>
-					<div class="container bg-success">
-						<p>Aparecerá aqui as informações</p>
-						<div id="recebe" style="display:none;"></div>
-						<button style="padding:7px; border:1px #808080 solid;" onclick="exibeNome('recebe')">Clique para ver seus dados!</button>
-						<script>
-							function exibeNome(recebe) {
-								var display = document.getElementById('recebe').style.display;
-								
-								if(display == "none"){
-									let nome = "<?php echo $_SESSION['nome'] ?>";
-									let saldo = "<?php echo $mostra1['saldo'] ?>";
-									recebeDados = document.getElementById('recebe');
-									recebeDados.innerHTML = "Seu nome: " + nome + "<br>" + "Seu saldo: " + saldo + "<br><br>";
-									document.getElementById('recebe').style.display = 'block';
-								}else{
-									document.getElementById('recebe').style.display = 'none';
-								}
-							}
-						</script>
-					</div>
-				</section>
-			</div>
+            <div class="container p-4 h-auto">
+                <div class="row pt-3 h-auto">
+                    <p class="display-4">COMÉRCIO</p>
+                </div>
+                <hr class="p-3" />
+                <div class="row">
+                    <div class="col">
+                        <h2 class="h2">O que é o comércio?</h2>
+                        <hr>
+                        <p>
+                            O comércio é o lugar onde você vai poder comprar os seus materiais para trabalhar!<br />
+                            Nele, você pode comprar e participar de disputas diversas. Use seu *barro para participar;
+                        </p>
+                    </div>
+                    <div class="col p-2">
+                        <p>Info</p>
+                    </div>
+                    <div class="col p-4">
+                        <div>
+                            <?php
+                                echo "<img class='w-50 fotoPerfil' src='./".$exibeFoto['path_perfil']."'>";
+                            ?>
+                            <h5>Adivinha a cor</h5>
+                            <p>
+                                Inscrição: R$ 1,00<br />
+                                20 de maio de 2022
+                            </p>
+                            <button class="btn btn-primary">CLIQUE PARA PARTICIPAR</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 			<div class="menu">
 				<nav class="sub-menu1"><button onclick="window.location='./inicio.php'"><img src="img/inicio_icon.png" alt="inicio"></button></nav>
 				<nav class="sub-menu1"><button onclick="window.location='./painelJogo.php';"><img src="img/salao_icon.png" alt="salao"></button></nav>
